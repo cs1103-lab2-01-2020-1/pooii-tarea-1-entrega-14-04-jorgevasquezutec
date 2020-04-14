cb00e28381d2feaa88ddef3a4761ae9c232aac32
jorgevasquezutec <jorge.vasquez@utec.edu.pe>
2020-04-13 19:16:22 -0500

    commit

---
+++ workspace/src/Tipos.h
@@ -0,0 +1,11 @@
+//
+// Created by Jorge Luis Vasquez on 13/04/2020.
+//
+
+#ifndef WORKSPACE_TIPOS_H
+#define WORKSPACE_TIPOS_H
+
+#include <iostream>
+using  namespace std;
+
+#endif //WORKSPACE_TIPOS_H
